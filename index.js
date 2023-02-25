2b04c2c322a55020a80de945e6042417785dae78
Michael Ebube <michaelebube18@gmail.com>
2023-02-25 01:40:52 +0100

    New Commit

--- index.js
+++ index.js
@@ -917,5 +917,126 @@ return result;
             }
             checkScope()
 
+             /* Declare a Read-only Variable with the const keyword */
+             function printManyTimes(str) {
+              "use strict"
+
+              const SENTENCE = str + " is cool"
+
+              // sentence = str + " is amazing"
+
+              for(let i = 0; i < str.length; i += 2) {
+                console.log(SENTENCE);
+              }
+            }
+            printManyTimes("freeCodeCamp")
+
+            /* Mutate an Array Declared with Const */ 
+            const s = [5, 7, 2 ]
+            function editInPlace() {
+              "use strict"
+
+              // s = [2, 5, 7]
+              s[0] = 2;
+              s[1] = 5;
+              s[2] = 7;
+            }
+            editInPlace()
+            console.log(s)
+
+            /* Prevent Object Mutation */
+            function freezeObj() {
+              "use strict"
+
+              const MATH_CONSTANTS = {
+                PI : 3.142
+              };
+
+             Object.freeze(MATH_CONSTANTS)
+
+              try {
+                MATH_CONSTANTS.PI = 99;
+              } catch( ex ) {
+                console.log(ex)
+              }
+              return MATH_CONSTANTS.PI;
+            }
+            const PI = freezeObj() 
+
+            /* Use Arrow functions to write concise Anonymous Functions */
+           /* const magic = function() {
+              return new Date();
+            } */
+
+            const magic = () => Date()
+
+            /*  Write Arrow functions with Parameters */
+           /* var myConcat = function(arr1, arr2) {
+              return arr1.concat(arr2);
+            }
+            console.log(myConcat([1, 2], [3, 4, 5])); */ 
+
+            const myConcat = (arr1, arr2) => arr1.concat(arr2)
+
+            console.log(myConcat([1, 2], [3, 4, 5])); 
+
+            /* Write Higher Order arrow Functions */
+            const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+            const squareList = (arr) => {
+              const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x)
+              return squaredIntegers;
+            } 
+            const squaredIntegers = squareList(realNumberArray);
+            console.log(squaredIntegers)
+
+            /* Write Higher Order Arrow Functions */
+            const increment = (function() {
+              return function increment(number, value = 1) {
+                return number + value;
+              };
+            }) ();
+            console.log(increment(5,2));
+            console.log(increment(5))
+
+            /* Use the rest operator with function parameters */
+            const sum = (function() {
+              return function sum(...args) {
+                return args.reduce((a,b) => a + b, 0);
+              };
+            })();
+            console.log(sum(1,2,3,4));
+
+             /* Use the spread operator to evaluate arrays In-Place */
+             const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+             let arr2;
+             (function() {
+              arr2 = [...arr1];
+              arr1[0] = 'potato'
+            }) ();
+            console.log(arr2);
+
+            /* Use Destructuring Assignment to Assign Variables from Objects */
+              var voxel = {x: 3.6, y: 7.4, z: 6.54};
+              /*  var x = voxel.x
+                  var y = voxel.y
+                  var z = voxel.z
+              */
+              const {x : a, y : b, z: c} = voxel;
+
+              const AVG_TEMPERATURES = {
+                today: 77.5,
+                tomorrow : 79
+              };
+
+              function getTempOfTmrw(avgTemperatures) {
+                "use strict"
+
+                const {tomorrow : tempOfTomorrow} = avgTemperatures
+
+                return tempOfTomorrow
+              }
+
+              console.log(getTempOfTmrw(AVG_TEMPERATURES));
+
 
 
